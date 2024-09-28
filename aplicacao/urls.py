f1142d1b84dc34389e61fc0a92c98bce34de511f
CelioJr12 <celiojunio878@gmail.com>
2024-09-28 11:17:21 -0300

    Update urls.py

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='aplicacao'
+
 urlpatterns = [
     path('home/', views.home, name="home"),
     path('adicionar/', views.adicionar, name="adicionar")
