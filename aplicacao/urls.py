57a7346036124792aa48f57b4d92e72a66cf0830
CelioJr12 <celiojunio878@gmail.com>
2024-09-28 16:19:42 -0300

    Update urls.py

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -6,5 +6,6 @@ app_name='aplicacao'
 urlpatterns = [
     path('', views.home, name="home"),
     path('adicionar/', views.adicionar, name="adicionar"),
-    path('visualizar_todos/', views.visualizar_todos, name="visualizar_todos")
+    path('visualizar_todos/', views.visualizar_todos, name="visualizar_todos"),
+    path('visualizar/<int:id>/', views.visualizar_um, name='visualizar_um')
 ]
