19ea0260dc26289d696e63c3754fa889b813d281
Pedro Caldas <pedro.caldas009@gmail.com>
2024-10-21 17:09:28 -0300

    iniciando testes automatizados

---
+++ cypress/e2e/adicionarReceita.cy.js
@@ -0,0 +1,13 @@
+describe('Adicionando receita', () => {
+    it('Adicionando uma receita com sucesso', () => {
+        cy.visit('/');
+    })
+
+    it('cenario2', () => {
+        //steps do cenario2
+    })
+
+    it('cenario3', () => {
+        //steps do cenario3
+    })
+})
