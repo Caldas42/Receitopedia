0c90343c15a51e89fc92e598b341b647c87319a2
nogsposito <vsan@cesar.school>
2024-10-21 17:04:26 -0300

    sistema de login consertado

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 # Create your views here.
 
-def registrar(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST) 
-        if form.is_valid():
-            form.save()
-            return redirect('login') 
-    else:
-        form = UserCreationForm() 
-    return render(request, 'registrar.html', {'form': form})
+def login_view(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('aplicacao:home')
+        else:
+            messages.error(request, 'Usuário ou senha incorretos.')
+
+    return render(request, 'login.html')
+
+def registrar_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if password == password_confirm:
+            try:
+                User.objects.create_user(username=username, password=password)
+                messages.success(request, 'Usuário registrado com sucesso!')
+                return redirect('login')  # Redirecionar para login após registro
+            except Exception as e:
+                messages.error(request, 'Erro ao criar usuário. Usuário já existe.')
+        else:
+            messages.error(request, 'As senhas não correspondem.')
+
+    return render(request, 'registrar.html')
+
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html')
