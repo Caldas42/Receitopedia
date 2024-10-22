47c5025f489ebafc92c14ba2a53e2048ea939931
Pedro Caldas <pedro.caldas009@gmail.com>
2024-10-21 21:18:35 -0300

    e2e do atualizar atualizado LOL

--- cypress/e2e/atualizarReceita.cy.js
+++ cypress/e2e/atualizarReceita.cy.js
@@ -1,74 +1,58 @@
 describe('Atualizando receita', () => {
-    it('atualizando receita com sucesso', () => {
+    it('Atualizando o nome com sucesso', () => {
         cy.visit('/');
         cy.get('#username').type('cypress')
         cy.get('#password').type('123abc')
         cy.get('button').click()
         cy.get('.card').click()
-        cy.get('#nome').type('chocolate quente')
+        cy.get('#nome').type('Chocolate quente')
         cy.get('#ingredientes').type('Chocolate e leite integral.')
         cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
         cy.get('#comentarios').type('Bom para tomar no frio.')
         cy.get('.button').click()
-        cy.get('h2').last().invoke('text').should('have.string', "chocolate quente")
-        cy.get('h2').last().then(($el) => {
-            const text = $el.text();
-            expect(text).to.contain("chocolate quente");
-            $el.click();
-        });
+        cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.get('h2').last().click()
         cy.get('#lapis').click()
-        cy.get('#nome').clear().type('chocolate frio')
-        cy.get('#modo_preparo').clear().type('Deixe o leite esfriar, acrescente o chocolate e misture bem.')
-        cy.get('#comentarios').clear().type('Bom para tomar no calor.')
+        cy.get('#nome').clear().type('Chocolate frio')
         cy.get('.button').click()
+        cy.get('h1').invoke('text').should('have.string', "Chocolate frio")
     })
 
-    it('Esquecendo de colocar os ingredientes logo depois de apagar os ingredientes antigos', () => {
+    it('Atualizando o modo de preparo com sucesso', () => {
         cy.visit('/');
         cy.get('#username').type('cypress')
         cy.get('#password').type('123abc')
         cy.get('button').click()
         cy.get('.card').click()
-        cy.get('#nome').type('chocolate quente')
+        cy.get('#nome').type('Chocolate quente')
         cy.get('#ingredientes').type('Chocolate e leite integral.')
         cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
         cy.get('#comentarios').type('Bom para tomar no frio.')
         cy.get('.button').click()
-        cy.get('h2').last().invoke('text').should('have.string', "chocolate quente")
-        cy.get('h2').last().then(($el) => {
-            const text = $el.text();
-            expect(text).to.contain("chocolate quente");
-            $el.click();
-        });
+        cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.get('h2').last().click()
         cy.get('#lapis').click()
-        cy.get('#nome').clear().type('chocolate frio')
+        cy.get('#modo_preparo').clear().type('Coloque o leite aquecido direto no chocolate na caneca e misture.')
+        cy.get('.button').click()
+        cy.get('#card > :nth-child(5)').invoke('text').should('have.string', "Coloque o leite aquecido direto no chocolate na caneca e misture.")
+    })
+
+    it('Esquecendo de colocar os ingredientes novos depois de ter apagado os ingredientes antigos', () => {
+        cy.visit('/');
+        cy.get('#username').type('cypress')
+        cy.get('#password').type('123abc')
+        cy.get('button').click()
+        cy.get('.card').click()
+        cy.get('#nome').type('Chocolate quente')
+        cy.get('#ingredientes').type('Chocolate e leite integral.')
+        cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
+        cy.get('#comentarios').type('Bom para tomar no frio.')
+        cy.get('.button').click()
+        cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.get('h2').last().click()
+        cy.get('#lapis').click()
         cy.get('#ingredientes').clear()
-        cy.get('#modo_preparo').clear().type('Deixe o leite esfriar, acrescente o chocolate e misture bem.')
-        cy.get('#comentarios').clear().type('Bom para tomar no calor.')
         cy.get('.button').click()
-    })
-
-    it('Esquecendo de colocar o modo de preaparo logo depois de ter apagado o modo de preparo antigo', () => {
-        cy.visit('/');
-        cy.get('#username').type('cypress')
-        cy.get('#password').type('123abc')
-        cy.get('button').click()
-        cy.get('.card').click()
-        cy.get('#nome').type('chocolate quente')
-        cy.get('#ingredientes').type('Chocolate e leite integral.')
-        cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
-        cy.get('#comentarios').type('Bom para tomar no frio.')
-        cy.get('.button').click()
-        cy.get('h2').last().invoke('text').should('have.string', "chocolate quente")
-        cy.get('h2').last().then(($el) => {
-            const text = $el.text();
-            expect(text).to.contain("chocolate quente");
-            $el.click();
-        });
-        cy.get('#lapis').click()
-        cy.get('#nome').clear().type('chocolate frio')
-        cy.get('#modo_preparo').clear()
-        cy.get('#comentarios').clear().type('Bom para tomar no calor.')
-        cy.get('.button').click()
+        cy.get('#ingredientes').invoke('text').should('have.string', "")
     })
 })
