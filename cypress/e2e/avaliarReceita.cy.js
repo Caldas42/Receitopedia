4ef79769840b1009c95e6000b3ee6e7a39c0d829
CelioJr12 <celiojunio878@gmail.com>
2024-10-21 21:36:01 -0300

    dois cenarios do teste de automação do avaliar

--- cypress/e2e/avaliarReceita.cy.js
+++ cypress/e2e/avaliarReceita.cy.js
@@ -1,13 +1,49 @@
 describe('Avaliar receita', () => {
-    it('cenario1', () => {
-        //steps do cenario1
+    it('Avaliar a receita com apenas uma estrela com sucesso', () => {
+        cy.visit('/');
+        cy.get('#username').type('cypress')
+        cy.get('#password').type('123abc')
+        cy.get('button').click()
+        cy.get('.card').click()
+        cy.get('#nome').type('chocolate quente')
+        cy.get('#ingredientes').type('Chocolate e leite integral.')
+        cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
+        cy.get('#comentarios').type('Bom para tomar no frio.')
+        cy.get('.button').click()
+        cy.get('h2').last().invoke('text').should('have.string', "chocolate quente")
+        cy.get('h2').last().then(($el) => {
+            const text = $el.text();
+            expect(text).to.contain("chocolate quente");
+            $el.click();
+        });
+        cy.get('[for="1-star"]').click()
+        cy.get('.rate-btn').click()
     })
 
-    it('cenario2', () => {
-        //steps do cenario2
+    it('Avaliar a receita com cinco estrelas com sucesso', () => {
+        it('Avaliar a receita com apena uma estrela com sucesso', () => {
+            cy.visit('/');
+            cy.get('#username').type('cypress')
+            cy.get('#password').type('123abc')
+            cy.get('button').click()
+            cy.get('.card').click()
+            cy.get('#nome').type('chocolate quente')
+            cy.get('#ingredientes').type('Chocolate e leite integral.')
+            cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
+            cy.get('#comentarios').type('Bom para tomar no frio.')
+            cy.get('.button').click()
+            cy.get('h2').last().invoke('text').should('have.string', "chocolate quente")
+            cy.get('h2').last().then(($el) => {
+                const text = $el.text();
+                expect(text).to.contain("chocolate quente");
+                $el.click();
+            });
+            cy.get('[for="5-stars"]').click()
+            cy.get('.rate-btn').click()
+        })
     })
 
-    it('cenario3', () => {
-        //steps do cenario3
+    it('Esquecendo de clicar nas estrelas e avaliando', () => {
+        
     })
 })
