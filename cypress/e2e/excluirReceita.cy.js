672e9abcf1978ab74cbb15f3647aed949c0c21de
Pedro Caldas <pedro.caldas009@gmail.com>
2024-10-22 18:18:24 -0300

    e2e excluir receita em andamento

--- cypress/e2e/excluirReceita.cy.js
+++ cypress/e2e/excluirReceita.cy.js
@@ -5,14 +5,15 @@ describe('Excluir receita', () => {
         cy.get('#password').type('123abc')
         cy.get('button').click()
         cy.get('.card').click()
-        cy.get('#nome').type('chocolate quente')
+        cy.get('#nome').type('Chocolate quente')
         cy.get('#ingredientes').type('Chocolate e leite integral.')
         cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
         cy.get('#comentarios').type('Bom para tomar no frio.')
         cy.get('.button').click()
-        cy.get('h2').last().invoke('text').should('have.string', "chocolate quente")
+        cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
         cy.get('h2').last().click()
         cy.get('.delete-btn').click()
+        cy.get('h2').last().contains('Chocolate quente').should('not.exist');
     })
 
     it('cenario2', () => {
