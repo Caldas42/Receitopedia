550cab297213fd3799e1adb5c7cb485608d7f7a0
Pedro Caldas <pedro.caldas009@gmail.com>
2024-10-22 17:12:42 -0300

    e2e de excluir e visualiizar editado

--- cypress/e2e/visualizarReceita.cy.js
+++ cypress/e2e/visualizarReceita.cy.js
@@ -5,17 +5,17 @@ describe('visualizar receita', () => {
         cy.get('#password').type('123abc')
         cy.get('button').click()
         cy.get('.card').click()
-        cy.get('#nome').type('chocolate quente')
+        cy.get('#nome').type('Chocolate quente')
         cy.get('#ingredientes').type('Chocolate e leite integral.')
         cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
         cy.get('#comentarios').type('Bom para tomar no frio.')
         cy.get('.button').click()
-        cy.get('h2').last().invoke('text').should('have.string', "chocolate quente")
-        cy.get('h2').last().then(($el) => {
-            const text = $el.text();
-            expect(text).to.contain("chocolate quente");
-            $el.click();
-        });
+        cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.get('h2').last().click()
+        cy.get('h1').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.get('#card > :nth-child(3)').last().invoke('text').should('have.string', "Chocolate e leite integral.")
+        cy.get('#card > :nth-child(5)').last().invoke('text').should('have.string', "Aqueça o leite, acrescente o chocolate e misture bem.")
+        cy.get('#card > :nth-child(7)').last().invoke('text').should('have.string', "Bom para tomar no frio.")
     })
 
     it('cenario2', () => {
