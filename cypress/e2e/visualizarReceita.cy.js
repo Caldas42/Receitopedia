a3a89d707e3560298d389eae94457e99889ce571
CelioJr12 <celiojunio878@gmail.com>
2024-10-23 12:07:27 -0300

    adicionado o cy.wait no e2e do add e view

--- cypress/e2e/visualizarReceita.cy.js
+++ cypress/e2e/visualizarReceita.cy.js
@@ -8,14 +8,18 @@ describe('visualizar receita', () => {
         cy.get('#nome').type('Chocolate quente')
         cy.get('#ingredientes').type('Chocolate e leite integral.')
         cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
+        cy.wait(2000)
         cy.get('#comentarios').type('Bom para tomar no frio.')
+        cy.wait(2000)
         cy.get('.button').click()
         cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.wait(2000)
         cy.get('h2').last().click()
         cy.get('h1').last().invoke('text').should('have.string', "Chocolate quente")
         cy.get('#card > :nth-child(3)').last().invoke('text').should('have.string', "Chocolate e leite integral.")
         cy.get('#card > :nth-child(5)').last().invoke('text').should('have.string', "Aqueça o leite, acrescente o chocolate e misture bem.")
         cy.get('#card > :nth-child(7)').last().invoke('text').should('have.string', "Bom para tomar no frio.")
+        cy.wait(5000)
     })
 
     it('Visualizando receita sem comentário com sucesso', () => {
@@ -27,12 +31,15 @@ describe('visualizar receita', () => {
         cy.get('#nome').type('Chocolate quente')
         cy.get('#ingredientes').type('Chocolate e leite integral.')
         cy.get('#modo_preparo').type('Aqueça o leite, acrescente o chocolate e misture bem.')
+        cy.wait(2000)
         cy.get('.button').click()
         cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.wait(3000)
         cy.get('h2').last().click()
         cy.get('h1').last().invoke('text').should('have.string', "Chocolate quente")
         cy.get('#card > :nth-child(3)').last().invoke('text').should('have.string', "Chocolate e leite integral.")
         cy.get('#card > :nth-child(5)').last().invoke('text').should('have.string', "Aqueça o leite, acrescente o chocolate e misture bem.")
+        cy.wait(5000)
     })
 
     it('Visualizando receita avaliada com sucesso', () => {
@@ -47,13 +54,17 @@ describe('visualizar receita', () => {
         cy.get('#comentarios').type('Bom para tomar no frio.')
         cy.get('.button').click()
         cy.get('h2').last().invoke('text').should('have.string', "Chocolate quente")
+        cy.wait(2000)
         cy.get('h2').last().click()
         cy.get('h1').last().invoke('text').should('have.string', "Chocolate quente")
         cy.get('#card > :nth-child(3)').last().invoke('text').should('have.string', "Chocolate e leite integral.")
         cy.get('#card > :nth-child(5)').last().invoke('text').should('have.string', "Aqueça o leite, acrescente o chocolate e misture bem.")
         cy.get('#card > :nth-child(7)').last().invoke('text').should('have.string', "Bom para tomar no frio.")
+        cy.wait(2000)
         cy.get('[for="5-stars"]').click()
+        cy.wait(2000)
         cy.get('.rate-btn').click()
+        cy.wait(2000)
         cy.get('.recipe-link').last().find('.stars > :nth-child(1)').should('have.attr', 'style').and('include', 'color:#ffc107');
         cy.get('.recipe-link').last().find('.stars > :nth-child(2)').should('have.attr', 'style').and('include', 'color:#ffc107');
         cy.get('.recipe-link').last().find('.stars > :nth-child(3)').should('have.attr', 'style').and('include', 'color:#ffc107');
@@ -65,5 +76,6 @@ describe('visualizar receita', () => {
         cy.get('.show-star-rating > :nth-child(3)').should('have.attr', 'style').and('include', 'color:#ffc107');
         cy.get('.show-star-rating > :nth-child(4)').should('have.attr', 'style').and('include', 'color:#ffc107');
         cy.get('.show-star-rating > :nth-child(5)').should('have.attr', 'style').and('include', 'color:#ffc107');
+        cy.wait(5000)
     })
 })
