7a10e8a0f92d5fb59824dc4482336c4cb0debc7c
nogsposito <vsan@cesar.school>
2024-10-27 13:43:31 -0300

    mensagem genérica de update

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -10,15 +10,17 @@ class HomeView(LoginRequiredMixin, View):
     login_url = 'login'
     
     def get(self, request):
+
         if request.user.is_authenticated:
-            Receita = receita.objects.filter(user=request.user) #refatorar depois
+            Receita = receita.objects.filter(user=request.user)
+
+            receitas_salvas = ReceitaSalva.objects.filter(user=request.user).values_list('receitaSalva', flat=True)
             receitas_salvas = receita.objects.filter(id__in=receitas_salvas)
 
             ctx = {
                 'todas_as_receitas': Receita,
                 'receitas_salvas': receitas_salvas,
             }
-            
             return render(request, 'home.html', ctx)
 
 class AddView(View):
