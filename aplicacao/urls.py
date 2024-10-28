b6884ad6ebd23c0cbecf3a351566d68bd613fef4
João Vitor Moraes <jvmb@cesar.school>
2024-10-28 17:02:09 -0300

    excluir receita da pasta sem css

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -14,9 +14,8 @@ urlpatterns = [
     path('minhas_pastas/', views.PastasView.as_view(), name='minhas_pastas'),
     path('adicionar_a_pasta/<int:receita_id>/', views.AdicionarReceitaAPastaView.as_view(), name='adicionar_a_pasta'),
     path('pasta/<int:pasta_id>/receitas/', views.ReceitasPastaView.as_view(), name='receitas_pasta'),
-<<<<<<< HEAD
-=======
     path('delete_all/', views.DeleteAllReceitasView.as_view(), name='delete_all_receitas'),
     path('timer/', views.TimerView.as_view(), name='timer'),
->>>>>>> 1298e60a8c57ec5d6214ee8cb4d50e51f02a95e6
+    path('remover_da_pasta/<int:receita_id>/', views.RemoverReceitaDaPastaView.as_view(), name='remover_da_pasta'),
+
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -135,3 +135,22 @@ class DeleteAllReceitasView(View):
 class TimerView(View):
     def get(self,request):
         return render(request, 'timer.html')
+    
+class RemoverReceitaDaPastaView(View):
+    def post(self, request, receita_id):
+        receita_obj = get_object_or_404(receita, id=receita_id)
+
+        # Armazena o ID da pasta antes de remover a receita
+        pasta_id = receita_obj.pasta.id if receita_obj.pasta else None
+
+        # Remover a receita da pasta
+        receita_obj.pasta = None
+        receita_obj.save()
+
+        messages.success(request, 'Receita removida da pasta com sucesso!')
+
+        # Redirecionar para a página da pasta se existir, caso contrário redirecionar para "minhas pastas"
+        if pasta_id:
+            return redirect('aplicacao:receitas_pasta', pasta_id=pasta_id)
+        else:
+            return redirect('aplicacao:minhas_pastas')  # ou 'aplicacao:home', dependendo da sua lógica
