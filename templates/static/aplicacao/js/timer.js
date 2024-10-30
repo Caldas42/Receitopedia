a7db40c702aed24fbb83742526e11060374fca16
CelioJr12 <celiojunio878@gmail.com>
2024-10-30 13:49:17 -0300

    correcoes feitas em timer

--- templates/static/aplicacao/js/timer.js
+++ templates/static/aplicacao/js/timer.js
@@ -4,11 +4,11 @@ let totalTime;
 document.getElementById('start-button').addEventListener('click', function() {
     const hoursInput = document.getElementById('hours-input');
     const minutesInput = document.getElementById('minutes-input');
+    const secondsInput = document.getElementById('seconds-input');
     const timerText = document.getElementById('timer-text');
     const circleInner = document.querySelector('.circle-inner');
     const startButton = document.getElementById('start-button');
     const cancelButton = document.getElementById('cancel-button');
-
 
     if (startButton.disabled) {
         return;
@@ -16,15 +16,14 @@ document.getElementById('start-button').addEventListener('click', function() {
 
     let hours = parseInt(hoursInput.value) || 0;
     let minutes = parseInt(minutesInput.value) || 0;
+    let seconds = parseInt(secondsInput.value) || 0;
 
-    totalTime = (hours * 3600) + (minutes * 60);
-
+    totalTime = (hours * 3600) + (minutes * 60) + seconds;
 
     if (totalTime <= 0) {
         alert('Por favor, insira um tempo válido.');
         return;
     }
-
 
     startButton.disabled = true;
     cancelButton.disabled = false;
@@ -35,9 +34,8 @@ document.getElementById('start-button').addEventListener('click', function() {
 
         updateTimerDisplay(totalTime);
 
-        const percentage = (totalTime / ((hours * 3600) + (minutes * 60))) * 100;
+        const percentage = (totalTime / ((hours * 3600) + (minutes * 60) + seconds)) * 100;
         circleInner.style.clipPath = `inset(${100 - percentage}% 0% 0% 0%)`;
-
 
         if (totalTime <= 0) {
             clearInterval(countdown);
@@ -50,10 +48,14 @@ document.getElementById('start-button').addEventListener('click', function() {
 });
 
 function updateTimerDisplay(time) {
-    const minutesLeft = Math.floor(time / 60);
+    const hoursLeft = Math.floor(time / 3600);
+    const minutesLeft = Math.floor((time % 3600) / 60);
     const secondsLeft = time % 60;
     const timerText = document.getElementById('timer-text');
-    timerText.textContent = `${minutesLeft < 10 ? '0' + minutesLeft : minutesLeft}:${secondsLeft < 10 ? '0' + secondsLeft : secondsLeft}`;
+
+    timerText.textContent = `${hoursLeft < 10 ? '0' + hoursLeft : hoursLeft}:${
+        minutesLeft < 10 ? '0' + minutesLeft : minutesLeft}:${
+        secondsLeft < 10 ? '0' + secondsLeft : secondsLeft}`;
 }
 
 document.getElementById('cancel-button').addEventListener('click', function() {
@@ -62,9 +64,10 @@ document.getElementById('cancel-button').addEventListener('click', function() {
     const startButton = document.getElementById('start-button');
     const cancelButton = document.getElementById('cancel-button');
 
-    timerText.textContent = "00:00";
+    timerText.textContent = "00:00:00";
     startButton.disabled = false;
     cancelButton.disabled = true;
     document.getElementById('hours-input').value = 0;
-    document.getElementById('minutes-input').value = 1;
+    document.getElementById('minutes-input').value = 0;
+    document.getElementById('seconds-input').value = 0;
 });
