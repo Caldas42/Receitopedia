d8b277b4a54a1a13fad7b0d146c599119ff31ab1
CelioJr12 <celiojunio878@gmail.com>
2024-11-11 16:27:19 -0300

    sugestoes e2e sendo implementado

--- cypress/e2e/sugestoes.cy.js
+++ cypress/e2e/sugestoes.cy.js
@@ -1,17 +1,37 @@
 describe('Sugestões de receita', () => {
     it('teste 1', () => {
+        cy.visit('/');
+        cy.wait(2000)
+        cy.get('#username').type('Receitopedia')
+        cy.get('#password').type('123abc')
+        cy.wait(2000)
+        cy.get('button').click()
+        cy.wait(5000)
+        cy.get('ul > :nth-child(4) > a').click()
+        cy.get('.fas').click()
 
     })
 
     it('Adicionando sugestão a minhas receitas', () => {
         cy.visit('/');
-        cy.get('#username').type('cypress2')
+        cy.wait(2000)
+        cy.get('#username').type('Receitopedia')
         cy.get('#password').type('123abc')
+        cy.wait(2000)
         cy.get('button').click()
+        cy.wait(5000)
         cy.get('ul > :nth-child(4) > a').click()
+        cy.get('.fas').click()
     })
 
     it(' teste 3', () => {
-
+        cy.visit('/');
+        cy.wait(2000)
+        cy.get('#username').type('Receitopedia')
+        cy.get('#password').type('123abc')
+        cy.wait(2000)
+        cy.get('button').click()
+        cy.wait(5000)
+        cy.get('.fas').click()
     })
 })
