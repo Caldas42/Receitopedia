40ef4265a224979c0857e2824878963becbf1716
CelioJr12 <celiojunio878@gmail.com>
2024-11-11 16:50:53 -0300

    um pouco e2e de sugestoes

--- cypress/e2e/sugestoes.cy.js
+++ cypress/e2e/sugestoes.cy.js
@@ -1,33 +1,37 @@
 describe('Sugestões de receita', () => {
-    it('teste 1', () => {
-        cy.visit('/');
-        cy.wait(2000)
-        cy.get('#username').type('Receitopedia')
-        cy.get('#password').type('123abc')
-        cy.wait(2000)
-        cy.get('button').click()
-        cy.wait(5000)
-        cy.get('ul > :nth-child(4) > a').click()
-        cy.get('.fas').click()
-
-    })
-
     it('Adicionando sugestão a minhas receitas', () => {
         cy.visit('/');
         cy.wait(2000)
-        cy.get('#username').type('Receitopedia')
+        cy.get('#username').type('cypress2')
+        cy.get('#password').type('123abc')
+        cy.wait(2000)
+        cy.get('button').click()
+        cy.wait(5000)
+        cy.get('ul > :nth-child(4) > a').click()
+        cy.wait(2000)
+        cy.get('.fas').click()
+        cy.wait(2000)
+        cy.get('h2').last().invoke('text').should('have.string', "Brigadeiro")
+    })
+
+    it('Tentando adicionar sugestão que já foi adicionada', () => {
+        cy.visit('/');
+        cy.wait(2000)
+        cy.get('#username').type('cypress2')
         cy.get('#password').type('123abc')
         cy.wait(2000)
         cy.get('button').click()
         cy.wait(5000)
         cy.get('ul > :nth-child(4) > a').click()
         cy.get('.fas').click()
+        
+
     })
 
-    it(' teste 3', () => {
+    it('Visualizar receita sugerida', () => {
         cy.visit('/');
         cy.wait(2000)
-        cy.get('#username').type('Receitopedia')
+        cy.get('#username').type('cypress2')
         cy.get('#password').type('123abc')
         cy.wait(2000)
         cy.get('button').click()
