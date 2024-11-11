89c5ce63b321243621d02b80bd171fdaeef30913
Pedro Caldas <pedro.caldas009@gmail.com>
2024-11-11 16:15:35 -0300

    comecando e2e sugestoes

--- cypress/e2e/sugestoes.cy.js
+++ cypress/e2e/sugestoes.cy.js
@@ -1,10 +1,14 @@
-describe('Adicionando sugestões', () => {
+describe('Sugestões de receita', () => {
     it('teste 1', () => {
 
     })
 
-    it('teste 2', () => {
-
+    it('Adicionando sugestão a minhas receitas', () => {
+        cy.visit('/');
+        cy.get('#username').type('cypress2')
+        cy.get('#password').type('123abc')
+        cy.get('button').click()
+        cy.get('ul > :nth-child(4) > a').click()
     })
 
     it(' teste 3', () => {
